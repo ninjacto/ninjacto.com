18a17126382611468c2364c5424f211ea70f42eb
Ramin <ramin.farmani@gmail.com>
2015-12-19 11:15:34 +0330

    post controller admin area

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,12 +1,12 @@
 <?php
 return [
+    'aliases' => [
+        '@frontend' => dirname(dirname(__DIR__)) . '/frontend',
+        '@frontendUrl' => 'https://www.ninjacto.com',
+        '@backend' => dirname(dirname(__DIR__)) . '/backend',
+        '@backendUrl' => 'https://admin5411.ninjacto.com',
+    ],
     'components' => [
-        'aliases' => [
-            '@frontend' => dirname(dirname(__DIR__)) . '/frontend',
-            '@frontendUrl' => 'https://www.ninjacto.com',
-            '@backend' => dirname(dirname(__DIR__)) . '/backend',
-            '@backendUrl' => 'https://admin5411.ninjacto.com',
-        ],
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=ninjacto',
