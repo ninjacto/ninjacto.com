aab93053aea02f59e75aba8d6d99d62178cfdfab
Ramin <ramin.farmani@gmail.com>
2015-12-22 13:00:27 +0330

    post controller admin area

--- common/config/main.php
+++ common/config/main.php
@@ -29,7 +29,7 @@ return [
     ],
     'components' => [
         'user' => [
-            'class' => 'common\models\User', // User must implement the IdentityInterface
+            'identityClass' => 'common\models\User', // User must implement the IdentityInterface
             'enableAutoLogin' => true,
             'loginUrl' => ['site/login'],
         ],
