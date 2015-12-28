f1687ae80e1c807c0c766aa8f6ca8d78957254dc
Ramin <ramin.farmani@gmail.com>
2015-12-28 11:00:50 +0330

    post controller admin area

--- frontend/themes/ruby/layouts/main.php
+++ frontend/themes/ruby/layouts/main.php
@@ -111,6 +111,21 @@ RubyAsset::register($this);
     </div>
 
     <?php $this->endBody() ?>
+    <!-- Piwik -->
+    <script type="text/javascript">
+        var _paq = _paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//piwik.ninjacto.com/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', 2]);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <noscript><p><img src="//piwik.ninjacto.com/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
 </body>
 </html>
 <?php $this->endPage() ?>
